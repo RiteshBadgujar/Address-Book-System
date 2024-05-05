758f363fa281d4c51f020403601472c7a8b851e5
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-05 10:41:12 +0530

    [ADDED]:Ritesh  The Well come Massage show

---
+++ src/com/ritesh/addressbook/AddressBook.java
@@ -0,0 +1,9 @@
+package com.ritesh.addressbook;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to Address Book Program");
+    }
+}
