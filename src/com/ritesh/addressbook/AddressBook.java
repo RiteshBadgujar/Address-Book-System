bfb243c25505a98faaa5fa61a6e4ff3cb5aaa53c
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-05 13:39:17 +0530

    [MODIFIED]:Ritesh UC-2 The add contact to Addressbook

--- src/com/ritesh/addressbook/AddressBook.java
+++ src/com/ritesh/addressbook/AddressBook.java
@@ -1,9 +1,48 @@
 package com.ritesh.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+    Contact contact = new Contact();
+    public void addContact()
+    {
+        System.out.println("Enter the First Name :");
+        contact.setFirstName(sc.next());
+        System.out.println("Enter the Last Name :");
+        contact.setLastName(sc.next());
+        System.out.println("Enter the Address :");
+        contact.setAddress(sc.next());
+        System.out.println("Enter the city :");
+        contact.setCity(sc.next());
+        System.out.println("Enter the State :");
+        contact.setState(sc.next());
+        System.out.println("Enter the zip code :");
+        contact.setZipcode(sc.next());
+        System.out.println("Enter the Phone Number :");
+        contact.setPhoneNumber(sc.next());
+        System.out.println("Enter the E-mail :");
+        contact.setEmail(sc.next());
+
+    }
+    public void showContact(){
+
+            System.out.println("FirstName :" + contact.getFirstName());
+            System.out.println("LastName :" + contact.getLastName());
+            System.out.println("Address :" + contact.getAddress());
+            System.out.println("City :" + contact.getCity());
+            System.out.println("State :" + contact.getState());
+            System.out.println("Zipcode :" + contact.getZipcode());
+            System.out.println("Phone Number :" + contact.getPhoneNumber());
+            System.out.println("Email :" + contact.getEmail());
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook = new AddressBook();
+
+        addressBook.addContact();
+        addressBook.showContact();
     }
 }
--- src/com/ritesh/addressbook/Contact.java
+++ src/com/ritesh/addressbook/Contact.java
@@ -1,5 +1,5 @@
 package com.ritesh.addressbook;
-
+import java.lang.String;
 public class Contact {
     private String  firstName;
     private String lastName;
@@ -9,4 +9,54 @@ public class Contact {
     private String zipcode;
     private String phoneNumber;
     private String email;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setState(String state){
+        this.state = state;
+    }
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getCity(){
+        return city;
+    }
+    public String getState(){
+        return state;
+    }
+    public String getZipcode(){
+        return zipcode;
+    }
+    public String getPhoneNumber(){
+
+        return phoneNumber;
+    }
+    public  String getEmail(){
+        return email;
+    }
 }
