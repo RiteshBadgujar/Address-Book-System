194a911e1be795d2087846bb80c296020cb609d9
RiteshBadgujar <riteshbadgujar0@gmail.com>
2024-05-05 10:50:37 +0530

    [MODIFIED]:Ritesh UC-1 The Create Contact

--- src/com/ritesh/addressbook/AddressBook.java
+++ src/com/ritesh/addressbook/AddressBook.java
@@ -3,7 +3,7 @@ package com.ritesh.addressbook;
 public class AddressBook {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Welcome to Address Book Program");
     }
 }
---
+++ src/com/ritesh/addressbook/Contact.java
@@ -0,0 +1,12 @@
+package com.ritesh.addressbook;
+
+public class Contact {
+    private String  firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zipcode;
+    private String phoneNumber;
+    private String email;
+}
